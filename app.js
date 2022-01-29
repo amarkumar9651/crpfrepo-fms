dc11982a6d27b46df05d39714e0806e98f1b8c82
Saurav Kumar <sauravpandey0022@gmail.com>
2022-01-29 13:09:03 +0530

    tried to add filter

--- app.js
+++ app.js
@@ -5,7 +5,9 @@ const mongoose = require('mongoose');
 const ejsMate=require('ejs-mate');
 const methodOverride = require('method-override')
 const session = require('express-session')
+const Vehicle=require('./models/vehicle')
 const ExpressError=require('./utils/ExpressError')
+const catchAsync=require('./utils/catchAsync')
 const vehicleRoutes=require('./routes/vehicles.js')
 mongoose.connect('mongodb://localhost:27017/fleet-managment', 
 { useNewUrlParser: true, 
@@ -38,6 +40,17 @@ app.use(session(sessionConfig))
 
 
 app.use('/vehicles',vehicleRoutes)
+app.post('/search/index',catchAsync(async(req,res,next)=>{
+const category=req.body.category;
+if(category!='')
+{
+   const vehicles=await Vehicle.find({category:category})
+   res.render('vehicles/index',{vehicles})
+}
+else{
+res.redirect('/vehicles')
+}
+}))
 app.get('/',(req,res)=>{
 res.render('home')
 })
